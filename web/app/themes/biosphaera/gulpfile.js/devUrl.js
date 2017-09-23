21b01f9eee03351ddc2785e4a6702ef3574ace01
Luca Castellone <luca.castellone@gmail.com>
2017-09-23 16:54:49 +0200

    Lasts

--- web/app/themes/biosphaera/gulpfile.js/devUrl.js
+++ web/app/themes/biosphaera/gulpfile.js/devUrl.js
@@ -1,1 +1,1 @@
-module.exports="http://192.168.1.18/biosphaera";
+module.exports="http://localhost/biosphaera";
--- web/app/themes/biosphaera/templates/content.php
+++ web/app/themes/biosphaera/templates/content.php
@@ -1,4 +1,6 @@
 <article <?php post_class(); ?>>
+ <?php  if(get_the_post_thumbnail( get_the_id(), 'medium' ) !==''){ ?> <div class="post-image"><?php the_post_thumbnail( get_the_id(), 'medium' ) ?></div>
+ <?php  } ?>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
